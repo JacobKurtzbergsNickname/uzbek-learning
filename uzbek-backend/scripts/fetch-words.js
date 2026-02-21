3ff5a8db1a97913df86a577b9f24668e51073ebd
Kirby <korbinian.brunner@web.de>
2026-02-21 13:06:30 +0100

    Removes (and rotates) auth credentials

--- uzbek-backend/scripts/fetch-words.js
+++ uzbek-backend/scripts/fetch-words.js
@@ -2,14 +2,22 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-var-requires */
-const axios = require("axios");
+import axios from "axios";
+import { getToken } from "./get-token.js";
+
+// Fetch the access token from Auth0
+const auth0Response = await getToken();
+console.log("Auth0 Response:", auth0Response);
+if (!auth0Response || !auth0Response.access_token) {
+  console.error("Failed to retrieve access token.");
+  process.exit(1);
+}
 
 const options = {
   method: "GET",
   url: "http://localhost:3421/words",
   headers: {
-    authorization:
-      "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InZXOW83d3h6QUFIbHJtdDRBTHFFSSJ9.eyJpc3MiOiJodHRwczovL2Rldi1hcDczaTQ4eWc1cnA0N3o4LnVzLmF1dGgwLmNvbS8iLCJzdWIiOiI2WlU1TWJHeWQ0cU5qS2xIelFtclpqREF4ZlBjb2w1WUBjbGllbnRzIiwiYXVkIjoidGFtZXJsYW5nLmRldiIsImlhdCI6MTc3MTUyNDM4NSwiZXhwIjoxNzcxNjEwNzg1LCJzY29wZSI6ImFkbWluIiwiZ3R5IjoiY2xpZW50LWNyZWRlbnRpYWxzIiwiYXpwIjoiNlpVNU1iR3lkNHFOaktsSHpRbXJaakRBeGZQY29sNVkiLCJwZXJtaXNzaW9ucyI6WyJhZG1pbiJdfQ.lMpn2DB9hFdWmZQdeywIZ5gIbTs693Ot1IAI700zu9xFLdfl8fNtTRaEJU8HTAfnRMsYmaJhhkGo1UIxw9mnw4NW6xZ7PD7eMGRPtVEaIdpBi7EfbkRkT4RI7XA_AHGviwKr5pY1BPxz6Kg2dSaLUxurDseukzHFQ3tDtOu1J3rhixf8nQ7wWFsXkAXNs8UO0Poh8Qp5Nxr-AfEheCqQSEmYsc5OnitXJCGgACkkmqzsRrX0YaVyyc_WmAlCSer-lJHQk2LMbpBI65O6FIBjFoJ1Uw9eio7xOkkxhliWkcyw9Xhqw1MLRT7KWhoL3BaKHBxYPwBjUnE1_pBo4g4DgQ",
+    authorization: `Bearer ${auth0Response.access_token}`,
   },
 };
 
--- uzbek-backend/scripts/get-token.js
+++ uzbek-backend/scripts/get-token.js
@@ -1,30 +1,24 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-async function getToken() {
+
+import axios from "axios";
+import { config } from "dotenv";
+config();
+
+export async function getToken() {
   try {
-    const response = await fetch(
-      "https://dev-ap73i48yg5rp47z8.us.auth0.com/oauth/token",
+    const response = await axios.post(
+      `https://${process.env.AUTH0_DOMAIN}/oauth/token`,
       {
-        method: "POST",
-        headers: { "content-type": "application/json" },
-        body: JSON.stringify({
-          client_id: "6ZU5MbGyd4qNjKlHzQmrZjDAxfPcol5Y",
-          client_secret:
-            "xLt1wfxCwJBAJHUTcMcVRMep7xhQ6tj7VaiOpT1qoYKA_lVhVp2F4G4qRVa0kN7v",
-          audience: "tamerlang.dev",
-          grant_type: "client_credentials",
-        }),
+        client_id: process.env.CLIENT_ID,
+        client_secret: process.env.CLIENT_SECRET,
+        audience: process.env.AUDIENCE,
+        grant_type: process.env.GRANT_TYPE,
       },
     );
-
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
-    }
-
-    const data = await response.json();
-    console.log(data);
+    return response.data ?? null;
   } catch (error) {
     console.error("Error fetching token:", error);
+    return null;
   }
 }
-
-await getToken();
